c8dad1e30293e2c7018ceb13160e783cac2575a5
Kaveon Smith <kaveon31@gmail.com>
2018-02-02 14:09:17 -0600

    added to handle network calls for exercise requests

---
+++ WBBackend/request_api/exercise_urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url
+from WBBackend.request_handlers import exercise_requests
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+#gonna need to get the post shit, may be dynamic
+urlpatterns = [
+    # still in testing phase,
+    url(r'getDefaultExercises',exercise_requests.getDefaultExercises),
+    url(r'getCustomExercises',exercise_requests.getCustomExercises),
+    url(r'getAllExercises',exercise_requests.getAllExercises),
+    url(r'createCustomExercise',exercise_requests.createCustomExercise),
+   # url(r'updateCustomExercise',exercise_requests.updateCustomExercise),
+    url(r'updateCustomExerciseImage',exercise_requests.updateCustomExerciseImage),
+    url(r'deleteCustomExercise',exercise_requests.deleteCustomExercise),
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+''' Things that need to be tested
+
+-- GET REQUESTS --
+# getDefaultExercises
+
+-- POST REQUESTS --
+#getCustomExercises
+#getAllExercises
+#createCustomExercise
+#updatCustomExercise
+updateCustomExerciseImage
+deleteCustomExercise
+'''

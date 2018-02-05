401f17ea1e700a242ebb378a9759a69001e7ef32
Kaveon Smith <kaveon31@gmail.com>
2018-02-04 20:29:18 -0600

    fixed some bugs,adding better response messages

--- WBBackend/request_handlers/profile_requests.py
+++ WBBackend/request_handlers/profile_requests.py
@@ -1,26 +1,53 @@
 from WBBackend.models import Profile
 # do not get rest of profile shit
+#setting max lifts should be an internal service
+
 def getProfileImage(request):
-    if request.method == 'GET':
-        profileId = request['profileId']
-        user_profile = Profile.objects.get(id=profileId)
+    if request.method == 'POST':
+        profileId = request.POST['profileId']
+        profile = Profile.objects.get(id=profileId)
         profile_image_path = profile.Profile_picture.url
-        json = {'id':user_profile.id,'profile_picture':profile_image_path}
+        json = {'id':profile.id,'profile_picture':Profile_image_path}
         return JsonResponse(json)
+    
+    json = {'error':True,'message':'The http request needs to be "POST" not "GET" ','RequestResponse':None}
+    return JsonResponse(json)
 
 def getMaxLifts(request):
-    if request.method == 'GET':
-        profileId = request['profileId']
-        user_profile = Profile.objects.get(id=profileId)
+    if request.method == 'POST':
+        profileId = request.POST['profileId']
+        profile = Profile.objects.get(id=profileId)
 
         max_lifts = []
-        max_bench = {'exercise_name':'Bench Press','max_lift':user_profile.max_bench.max_bench,'unit':user_profile.max_bench.unit}
-        max_squat = {'exercise_name':'Squat','max_lift':user_profile.max_squat.max_squat,'unit':user_profile.max_squat.unit}
-        max_dead_lift = {'exercise_name':'Dead-Lift','max_lift':user_profile.max_squat.max_dead_lift,'unit':user_profile.max_dead_lift.unit}
+        max_bench = {'exercise_name':'Bench Press','max_lift':profile.max_bench.max_bench,'unit':profile.max_bench.unit}
+        max_squat = {'exercise_name':'Squat','max_lift':profile.max_squat.max_squat,'unit':profile.max_squat.unit}
+        max_dead_lift = {'exercise_name':'Dead-Lift','max_lift':profile.max_squat.max_dead_lift,'unit':profile.max_dead_lift.unit}
         max_lifts.append(max_bench)
         max_lifts.append(max_squat)
         max_lifts.append(max_dead_lift)
 
         json = {'error':False,'message':'Request successfully completed','RequestResponse':max_lifts}
         return JsonResponse(json)
-        
+    
+    json = {'error':True,'message':'The http request needs to be "POST" not "GET" ','RequestResponse':None}
+    return JsonResponse(json)
+
+def updateProfileImage(request):
+    if request.method == 'POST':
+        if request.FILES != {}:
+            file_name = request.GET['file_name']
+            profileId = request.GET['profileId']
+            user_profile = Profile.objects.get(id=profileId)
+            user_profile.profile_picture = request.FILES[file_name]
+        else:
+            file_path = request.POST['file_path']
+            profileId = request.POST['profileId']
+            user_profile = Profile.objects.get(id=profileId)
+            user_profile.profile_picture = file_path
+            user_profile.save()
+
+         json = {'error':False,'message':'Request successfully completed','RequestResponse':'Profile Image Changed!'}
+        return JsonResponse(json)
+    
+    json = {'error':True,'message':'The http request needs to be "POST" not "GET" ','RequestResponse':None}
+    return JsonResponse(json)

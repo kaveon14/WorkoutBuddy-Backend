225cd8ecb24e7c24fba661f766eec13d7551c23c
Kaveon Smith <kaveon31@gmail.com>
2018-02-24 15:15:10 -0600

    fixed a smaa bug

--- WBBackend/request_api/exercise_urls.py
+++ WBBackend/request_api/exercise_urls.py
@@ -3,30 +3,16 @@ from WBBackend.request_handlers import exercise_requests
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-#gonna need to get the post shit, may be dynamic
 urlpatterns = [
     # still in testing phase,
-    url(r'getDefaultExercises',exercise_requests.getDefaultExercises),
-    url(r'getCustomExercises',exercise_requests.getCustomExercises),
-    url(r'getAllExercises',exercise_requests.getAllExercises),
-    url(r'createCustomExercise',exercise_requests.createCustomExercise),
-    url(r'updateCustomExercise',exercise_requests.updateCustomExercise),
-    url(r'updateCustomExerciseImage',exercise_requests.updateCustomExerciseImage),
-    url(r'deleteCustomExercise',exercise_requests.deleteCustomExercise),
+    url(r'^getDefaultExercises',exercise_requests.getDefaultExercises),
+    url(r'^getDefaultExercise',exercise_requests.getDefaultExercise),
+    url(r'^getCustomExercise',exercise_requests.getCustomExercise),
+    url(r'^getCustomExercises',exercise_requests.getCustomExercises),
+    url(r'^getAllExercises',exercise_requests.getAllExercises),
+    url(r'^createCustomExercise',exercise_requests.createCustomExercise),
+    url(r'^updateCustomExercise',exercise_requests.updateCustomExercise),
+    url(r'setCustomExerciseImage',exercise_requests.setCustomExerciseImage),
+    url(r'^u',exercise_requests.updateCustomExerciseImage),
+    url(r'^deleteCustomExercise',exercise_requests.deleteCustomExercise),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-''' Things that need to be tested
-
--- GET REQUESTS --
-# getDefaultExercises
-
--- POST REQUESTS --
-#getCustomExercises
-#getAllExercises
-#createCustomExercise
-#updatCustomExercise
-#updateCustomExerciseImage
-#deleteCustomExercise
-'''

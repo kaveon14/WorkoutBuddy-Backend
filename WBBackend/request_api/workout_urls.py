e53122f68da3695651e445d5b75dbebf855af09f
Kaveon Smith <kaveon31@gmail.com>
2018-02-28 12:24:45 -0600

    added urls for delete main/sub workouts

--- WBBackend/request_api/workout_urls.py
+++ WBBackend/request_api/workout_urls.py
@@ -4,12 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'getMainWorkouts',workout_requests.getMainWorkouts),
-    url(r'getSubWorkouts',workout_requests.getSubWorkouts),
-    url(r'getSubWorkoutExercises',workout_requests.getSubWorkoutExercises),
-    url(r'getCompletedWorkouts',workout_requests.getCompletedWorkouts),
-    url(r'createMainWorkout',workout_requests.createMainWorkout),
-    url(r'createSubWorkout',workout_requests.createSubWorkout),
+    url(r'^getMainWorkouts',workout_requests.getMainWorkouts),
+    url(r'^getSubWorkouts',workout_requests.getSubWorkouts),
+    url(r'^getSubWorkoutExercises',workout_requests.getSubWorkoutExercises),
+    url(r'^getCompletedWorkouts',workout_requests.getCompletedWorkouts),
+    url(r'^createMainWorkout',workout_requests.createMainWorkout),
+    url(r'^createSubWorkout',workout_requests.createSubWorkout),
+    url(r'^deleteMainWorkout',workout_requests.deleteMainWorkout),
+    url(r'^deleteSubWorkout',workout_requests.deleteSubWorkout),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 '''

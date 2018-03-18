517d1ccf78f1980038752d752d4f0ace0ceddddf
Kaveon Smith <kaveon31@gmail.com>
2018-03-18 13:45:33 -0500

    updated urls

--- AdminBackend/urls.py
+++ AdminBackend/urls.py
@@ -1,37 +1,22 @@
-"""WorkoutBuddySite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from AdminBackend import views as core_views
-from django.conf import settings
 from django.conf.urls.static import static
-import AdminBackend.views as view
-
-from WBBackend.request_handlers import profile_requests
-from rest_framework_jwt.views import obtain_jwt_token
+from django.conf.urls import url, include
+from django.conf import settings
+from .views import ApiEndpoint
+from .views import secret_page
 
 urlpatterns = [
-    url(r'android_login',view.android_login),
-    url(r'^signup/$', core_views.signup, name='signup'),
-    url(r'^login', profile_requests.log_user_in, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'exercises/', include('WBBackend.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'testExerciseApi/',include('WBBackend.request_api.exercise_urls')),
-    url(r'testWorkoutApi/',include('WBBackend.request_api.workout_urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
-
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url(r'^api/hello', ApiEndpoint.as_view()),
+    url(r'^secret$', secret_page, name='secret'),
+    url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^login', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^exercises/', include('WBBackend.urls')),
+    url(r'^testExerciseApi/',include('WBBackend.request_api.exercise_urls')),
+    url(r'^testWorkoutApi/',include('WBBackend.request_api.workout_urls')),
+    url(r'^testProgressPhotoApi/',include('WBBackend.request_api.progress_photo_urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

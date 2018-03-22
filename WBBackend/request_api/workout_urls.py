b988c1454469996c7e769bbdcccfdf73bb524f50
Kaveon Smith <kaveon31@gmail.com>
2018-03-22 09:33:18 -0500

    updated urls

--- WBBackend/request_api/workout_urls.py
+++ WBBackend/request_api/workout_urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^getMainWorkouts',workout_requests.getMainWorkouts),
-    url(r'^getSubWorkouts',workout_requests.getSubWorkouts),
-    url(r'^getSubWorkoutExercises',workout_requests.getSubWorkoutExercises),
+    url(r'^getSubWorkout',workout_requests.getSubWorkout),
+    url(r'^getAllSubWorkouts',workout_requests.getSubWorkouts),
+    url(r'^getExerciseGoals', workout_requests.getSubWorkoutExerciseGoals),
+    url(r'^getSingleExGoal', workout_requests.getSubWorkoutExerciseGoal),
     url(r'^getCompletedWorkouts',workout_requests.getCompletedWorkouts),
     url(r'^createMainWorkout',workout_requests.createMainWorkout),
     url(r'^createSubWorkout',workout_requests.createSubWorkout),
@@ -14,17 +16,7 @@ urlpatterns = [
     url(r'^deleteSubWorkout',workout_requests.deleteSubWorkout),
     url(r'^updateMainWorkoutName',workout_requests.updateMainWorkoutName),
     url(r'^updateSubWorkoutName',workout_requests.updateSubWorkoutName),
-    url(r'^updateSubWorkoutExerciseGoals',workout_requests.updateSubWorkoutExerciseGoals),
     url(r'^addExerciseGoals',workout_requests.addSubWorkoutExerciseGoals),
+    url(r'^updateExerciseGoals',workout_requests.updateExerciseGoal),
     url(r'^deleteExerciseGoals',workout_requests.deleteSubWorkoutExerciseGoals),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-'''
--- needs to be tested --
-#getMainWorkouts
-#getSubWorkouts
-#getSubWorkoutExercises
-#getCompletedWorkouts
-#createMainWorkoutE
-createSubWorkout# must be tested differently, possibly android app or use php to send post request
-'''
